08010498516ed993003ced4d768ad947836668d0
abdallah <abdallahantony55.aa@gmail.com>
2023-01-31 18:30:16 +0300

    Add class Person,Student and Teacher

---
+++ .github/workflows/Teacher.rb
@@ -0,0 +1,16 @@
+require_relative 'person'
+
+class Teacher < Person
+    attr_reader :specialization
+  
+    def initialize(specialization:, name: "Unknown", age:, parent_permission: true)
+      super(name: name, age: age, parent_permission: parent_permission)
+      @specialization = specialization
+    end
+  
+    def can_use_services?
+      true
+    end
+end
+
+
